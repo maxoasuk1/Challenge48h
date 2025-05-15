3cc4ceb269fe9e8479abb91e1346b7fcba367076
fl-hugo <150928384+fl-hugo@users.noreply.github.com>
2025-05-15 16:50:10 +0200

    Add timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CountdownTimer : MonoBehaviour
 {
@@ -8,8 +9,19 @@ public class CountdownTimer : MonoBehaviour
 
     public TextMeshProUGUI timerText;
 
-    private void Start()
+    public static CountdownTimer instance;
+
+    private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad((gameObject.transform.root.gameObject));
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         currentTime = totalTime;
     }
 
@@ -35,5 +47,24 @@ public class CountdownTimer : MonoBehaviour
         int seconds = Mathf.FloorToInt(currentTime % 60f);
 
         timerText.text = $"{minutes:00}:{seconds:00}";
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        GameObject timerGO = GameObject.Find("TimerText");
+        if (timerGO != null)
+        {
+            timerText = timerGO.GetComponent<TextMeshProUGUI>();
+        }
     }
 }
